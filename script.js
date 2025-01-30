42ca637f58174ac707d2b456ec85165b2d04b8ac
SkyLostTV <kikitrott1504@gmail.com>
2025-01-30 21:54:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-// Liste de films dynamiques
-const movies = [
-    { title: "Inception", image: "https://source.unsplash.com/200x300/?movie,1" },
-    { title: "Interstellar", image: "https://source.unsplash.com/200x300/?movie,2" },
-    { title: "Dune", image: "https://source.unsplash.com/200x300/?movie,3" },
-    { title: "Avatar", image: "https://source.unsplash.com/200x300/?movie,4" },
-    { title: "The Dark Knight", image: "https://source.unsplash.com/200x300/?movie,5" },
-    { title: "Tenet", image: "https://source.unsplash.com/200x300/?movie,6" },
-];
+// Importer Firebase
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/analytics";
 
-// Sélectionne l'élément HTML où on va afficher les films
-const container = document.getElementById("movies-container");
+// 📌 CONFIGURATION FIREBASE (colle tes infos ici)
+const firebaseConfig = {
+    apiKey: "AIzaSyBipzIDuYDTfwnyFsWBW67aib1UXXQjvX8",
+    authDomain: "popflix-de65c.firebaseapp.com",
+    projectId: "popflix-de65c",
+    storageBucket: "popflix-de65c.firebasestorage.app",
+    messagingSenderId: "219558339530",
+    appId: "1:219558339530:web:1844367f37ea2556a4a533"
+    measurementId: "G-6RM4111K6Q"
+};
 
-// Génère les films dynamiquement
-movies.forEach(movie => {
-    const img = document.createElement("img");
-    img.src = movie.image;
-    img.alt = movie.title;
-    img.classList.add("movie-poster");
-    container.appendChild(img);
-});
+// Initialisation Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Récupérer les films depuis Firebase
+async function fetchMovies() {
+    const querySnapshot = await getDocs(collection(db, "movies"));
+    const container = document.getElementById("movies-container");
+    container.innerHTML = ""; // On vide avant de remplir
+
+    querySnapshot.forEach((doc) => {
+        const movie = doc.data();
+        const img = document.createElement("img");
+        img.src = movie.image;
+        img.alt = movie.title;
+        img.classList.add("movie-poster");
+        container.appendChild(img);
+    });
+}
+
+// Charger les films au démarrage
+fetchMovies();
